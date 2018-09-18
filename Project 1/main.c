d04d39844e724f821720baf23f34b233c0b972a5
romansparta <chenwill98@gmail.com>
2018-09-18 16:00:21 -0400

    Finished core code

--- Project 1/main.c
+++ Project 1/main.c
@@ -2,67 +2,60 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 #include <errno.h>
-extern int errno;
-
 void error();
+
 int main(int argc, char *argv[]) {
 
-        int c = 0, err = 0, fd_out = 0, fd_in = 0, buf_val = 0;
-        int bflag = 0, oflag = 0, bytes_r = 0, bytes_w = 0;
-        char *bname, *oname;
-        char *buf = (char *) malloc(100*sizeof(char));
+        int c = 0, fd_out = 0, fd_in = 0, buf_size = 8192;
+        int bytes_r = 1, bytes_w = 1;
         while ((c = getopt(argc, argv, "b:o:")) != -1)
                 switch (c) {
                 case 'b':
-                        bflag = 1;
-                        bname = optarg;
+                        buf_size = atoi(optarg);
                         break;
                 case 'o':
-                        oflag = 1;
-                        oname = optarg;
+                        fd_out = open(optarg, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                        if (fd_out < 0) {
+                                error(1, "creating or reading the", optarg, "null");
+                                return -1;
+                        }
                         break;
                 case '?':
-                        err = 1;
+                        return -1;
                         break;
                 }
-        if (err == 1)
-                return -1;
-
-        if (oflag == 1) {
-                fd_out = open(oname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                if (fd_out < 0) {
-                        error(1, "creating or reading the", oname, "null");
-                        return -1;
-                }
-        }
-        if (bflag == 1) {
-                buf_val = atoi(bname);
-                if (buf_val == 0) {
-                        error(0, "converting string to integer with the", "buffer value", "This string is not a number");
-                        return -1;
-                }
-                buf = (char *) malloc(atoi(bname)*sizeof(char));
-        }
-
-        if (optind < argc) // if there are infiles, it loops
+        char buf[buf_size];
+        if (optind < argc) // if there are infiles, it loops until the end of the argument to take in all the arguments
                 for (; optind < argc; optind++) {
-                        printf("%s\n", argv[optind]);
-                        if (argv[optind] != "-") {
-                                printf("heyo");
+                        bytes_r = 1;
+                        if (strcmp(argv[optind], "-") != 0) {
                                 fd_in = open(argv[optind], O_RDONLY);
-                                bytes_w = write(fd_out, buf, read(fd_in, buf, 100));
+                                while (bytes_r != 0) {
+                                        bytes_r = read(fd_in, buf, sizeof(buf));
+                                        bytes_w = write(fd_out, buf, bytes_r);
+                                }
                                 if (fd_in < 0) {
                                         error(0, "opening the file", argv[optind], "File does not exist");
                                         return -1;
                                 }
                         } else {
-                                printf("heyo-");
-                                //bytes_w = write(fd_out, buf, read(0, buf, 100));
+                                while (bytes_r != 0) {
+                                        bytes_r = read(0, buf, sizeof(buf));
+                                        bytes_w = write(fd_out, buf, bytes_r);
+                                }
+                                if (bytes_w < 0) {
+                                        error(1, "writing to ", "standard output", "null");
+                                        return -1;
+                                }
                         }
                 }
         else { // if there aren't any infiles, it just pulls from standard input
-                bytes_w = write(fd_out, buf, read(0, buf, 100));
+                while (bytes_r != 0) {
+                        bytes_r = read(0, buf, sizeof(buf));
+                        bytes_w = write(fd_out, buf, bytes_r);
+                }
                 if (bytes_w < 0) {
                         error(1, "writing to ", "standard output", "null");
                         return -1;
