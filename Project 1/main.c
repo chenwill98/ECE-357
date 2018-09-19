546ead95646d718cd6cac8d4077cf0d64a96da7f
romansparta <chenwill98@gmail.com>
2018-09-19 15:28:31 -0400

    Finished all the code, including error handling!

--- Project 1/main.c
+++ Project 1/main.c
@@ -4,70 +4,79 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
-void error();
+void check_error(int val, char *obj, char c);
 
 int main(int argc, char *argv[]) {
-
         int c = 0, fd_out = 0, fd_in = 0, buf_size = 8192;
         int bytes_r = 1, bytes_w = 1;
+        char *oname = "standard output";
         while ((c = getopt(argc, argv, "b:o:")) != -1)
                 switch (c) {
                 case 'b':
                         buf_size = atoi(optarg);
                         break;
                 case 'o':
-                        fd_out = open(optarg, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                        if (fd_out < 0) {
-                                error(1, "creating or reading the", optarg, "null");
-                                return -1;
-                        }
+                        oname = optarg;
+                        fd_out = open(oname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                        check_error(fd_out, oname, 'o');
                         break;
                 case '?':
                         return -1;
                         break;
                 }
         char buf[buf_size];
-        if (optind < argc) // if there are infiles, it loops until the end of the argument to take in all the arguments
+        if (optind < argc) {// if there are infiles, it loops until the end of the argument to take in all the arguments
                 for (; optind < argc; optind++) {
                         bytes_r = 1;
                         if (strcmp(argv[optind], "-") != 0) {
                                 fd_in = open(argv[optind], O_RDONLY);
+                                check_error(fd_in, argv[optind], 'o');
                                 while (bytes_r != 0) {
                                         bytes_r = read(fd_in, buf, sizeof(buf));
+                                        check_error(bytes_r, argv[optind], 'r');
                                         bytes_w = write(fd_out, buf, bytes_r);
-                                }
-                                if (fd_in < 0) {
-                                        error(0, "opening the file", argv[optind], "File does not exist");
-                                        return -1;
+                                        check_error(bytes_w, oname, 'w');
                                 }
                         } else {
                                 while (bytes_r != 0) {
                                         bytes_r = read(0, buf, sizeof(buf));
+                                        check_error(bytes_r, argv[optind], 'r');
                                         bytes_w = write(fd_out, buf, bytes_r);
-                                }
-                                if (bytes_w < 0) {
-                                        error(1, "writing to ", "standard output", "null");
-                                        return -1;
+                                        check_error(bytes_w, oname, 'w');
                                 }
                         }
                 }
-        else { // if there aren't any infiles, it just pulls from standard input
+        }
+        else {         // if there aren't any infiles, it just pulls from standard input
                 while (bytes_r != 0) {
                         bytes_r = read(0, buf, sizeof(buf));
+                        check_error(bytes_r, argv[optind], 'r');
                         bytes_w = write(fd_out, buf, bytes_r);
-                }
-                if (bytes_w < 0) {
-                        error(1, "writing to ", "standard output", "null");
-                        return -1;
+                        check_error(bytes_w, oname, 'w');
                 }
         }
+        check_error(close(fd_in), argv[optind], 'c');
+        check_error(close(fd_out), oname, 'c');
 }
 
-void error(int p_err, char *action, char *subject, char *custom_error) {
+void check_error(int val, char *obj, char c) { //4 cases: error opening, reading, writing, and closing
         char err_msg[1024];
-        snprintf(err_msg, sizeof(err_msg), "Error %s %s", action, subject);
-        if (p_err == 1)
+        if (val < 0) {
+                switch (c) {
+                case 'o':
+                        snprintf(err_msg, sizeof(err_msg), "Error opening file %s", obj);
+                        break;
+                case 'r':
+                        snprintf(err_msg, sizeof(err_msg), "Error reading file %s", obj);
+                        break;
+                case 'w':
+                        snprintf(err_msg, sizeof(err_msg), "Error writing to file %s", obj);
+                        break;
+                case 'c':
+                        snprintf(err_msg, sizeof(err_msg), "Error closing file %s", obj);
+                        break;
+                }
                 perror(err_msg);
-        else
-                printf("%s: %s", err_msg, custom_error);
+                exit(-1);
+        }
 }
