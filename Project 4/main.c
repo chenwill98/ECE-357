89c9efbb67b2e0f61d85876034655c7437edc597
romansparta <chenwill98@gmail.com>
2018-11-13 01:46:19 -0500

    In progress

--- Project 4/main.c
+++ Project 4/main.c
@@ -1,69 +1,73 @@
+//William Chen
+//Professor Hakner
+//ECE-357 Operating Systems
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/wait.h>
+
+char err_msg[1024];
+int files_proc = 0;
+long bytes_proc = 0;
+
+void handler(int s) { //writes the total files and bytes processed before exiting
+        fprintf(stderr, "Files Processed: %i, Bytes Processed: %l.\n", files_proc, bytes_proc);
+        exit(0);
+}
+
 void check_error(int val, char *obj, char c);
 
 int main(int argc, char *argv[]) {
-        int c = 0, fd_out = 0, fd_in = 0, buf_size = 8192;
+        struct sigaction sa;
+        int fd_out = 0, fd_in = 0, buf_size = 4096;
         int bytes_r = 1, bytes_w = 1;
+        char *pattern, *buf;
         char *oname = "standard output";
-        while ((c = getopt(argc, argv, "b:o:")) != -1)
-                switch (c) {
-                case 'b':
-                        buf_size = atoi(optarg);
-                        break;
-                case 'o':
-                        oname = optarg;
-                        fd_out = open(oname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                        check_error(fd_out, oname, 'o');
-                        break;
-                case '?':
-                        return -1;
-                        break;
-                }
-        char buf[buf_size];
-        if (optind < argc) {// if there are infiles, it loops until the end of the argument to take in all the arguments
+
+        sa.sa_handler=handler; //handles the signal properly
+        sa.sa_flags=0;
+        sigemptyset(&sa.sa_mask);
+        if (sigaction(SIGINT, &sa, 0) == -1) {
+                snprintf(err_msg, sizeof(err_msg), "Error changing the signal disposition:");
+                perror(err_msg);
+                return -1;
+        }
+
+        if (argc < 3) { //if there are fewer than 3 arguments catgrepmore is being used wrong
+                fprintf(stderr, "Correct format: catgrepmore pattern infline1 [...infile2...]\n");
+                return -1;
+        }
+        pattern = argv[optind++];
+
+        if (!(buf = malloc(buf_size * sizeof(char)))) { //checks to see if memory allocation succeeded
+                snprintf(err_msg, sizeof(err_msg), "Error allocating memory for default buffer size:");
+                perror(err_msg);
+                return -1;
+        }
+
+        if (optind < argc) { //loops until the end of the argument to take in all the arguments
                 for (; optind < argc; optind++) {
                         bytes_r = 1;
-                        if (strcmp(argv[optind], "-") != 0) {
-                                fd_in = open(argv[optind], O_RDONLY);
-                                check_error(fd_in, argv[optind], 'o');
-                                while (bytes_r != 0) {
-                                        bytes_r = read(fd_in, buf, sizeof(buf));
-                                        check_error(bytes_r, argv[optind], 'r');
-                                        bytes_w = write(fd_out, buf, bytes_r);
-                                        check_error(bytes_w, oname, 'w');
-                                        check_error(bytes_w - bytes_r, oname, 'p');
-                                }
-                        } else {
-                                while (bytes_r != 0) {
-                                        bytes_r = read(0, buf, sizeof(buf));
-                                        check_error(bytes_r, argv[optind], 'r');
-                                        bytes_w = write(fd_out, buf, bytes_r);
-                                        check_error(bytes_w, oname, 'w');
-                                        check_error(bytes_w - bytes_r, oname, 'p');
-                                }
+                        fd_in = open(argv[optind], O_RDONLY);
+                        check_error(fd_in, argv[optind], 'o');
+                        while (bytes_r != 0) {
+                                bytes_r = read(fd_in, buf, sizeof(buf));
+                                check_error(bytes_r, argv[optind], 'r');
+                                bytes_w = write(fd_out, buf, bytes_r);
+                                check_error(bytes_w, oname, 'w');
+                                check_error(bytes_w - bytes_r, oname, 'p');
                         }
+                        check_error(close(fd_out), oname, 'c');
                 }
         }
-        else { // if there aren't any infiles, it just pulls from standard input
-                while (bytes_r != 0) {
-                        bytes_r = read(0, buf, sizeof(buf));
-                        check_error(bytes_r, argv[optind], 'r');
-                        bytes_w = write(fd_out, buf, bytes_r);
-                        check_error(bytes_w, oname, 'w');
-                        check_error(bytes_w - bytes_r, oname, 'p');
-                }
-        }
-        check_error(close(fd_in), argv[optind], 'c');
-        check_error(close(fd_out), oname, 'c');
 }
 
 void check_error(int val, char *obj, char c) { //4 cases: error opening, reading, writing, and closing
-        char err_msg[1024];
+
         if (val < 0) {
                 switch (c) {
                 case 'o':
