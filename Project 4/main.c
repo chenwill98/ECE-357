0299db16258c166ce74223a8406a1eaed8cb9864
romansparta <chenwill98@gmail.com>
2018-11-14 02:19:48 -0500

    Final commit + formatting

--- Project 4/main.c
+++ Project 4/main.c
@@ -16,31 +16,16 @@ int files_proc = 0;
 long bytes_proc = 0;
 
 void handler(int s) { //writes the total files and bytes processed before exiting
-        fprintf(stderr, "Files Processed: %i\nBytes Processed: %l\n", files_proc, bytes_proc);
+        fprintf(stderr, "Files Processed: %i\nBytes Processed: %ld\n", files_proc, bytes_proc);
         exit(0);
 }
 
-int pipeIO(int pipe[], const int fd_to_redirect) {
-        /* connect stding to pipe and close the pipe's file descriptors */
-        if (fd_to_redirect == STDIN_FILENO) {
-                if (dup2(pipe[0], STDIN_FILENO) == -1) {
-                        fprintf(stderr, "Error redirecting input: %s\n", strerror(errno));
-                        return -1;
-                }
-        }
-        else {
-                if (dup2(pipe[1], STDOUT_FILENO) == -1) {
-                        fprintf(stderr, "Error redirecting output: %s\n", strerror(errno));
-                        return -1;
-                }
-        }
-        return 0;
-}
+int close_pipes(int p1[], int p2[]);
+int pipe_ops(int fd, int pipe[]);
 
 int main(int argc, char *argv[]) {
         struct sigaction sa;
         int fd_in = 0, buf_size = 4096;
-        int bytes_r = 1, bytes_w = 1;
         char *pattern, *buf;
 
         sa.sa_handler=handler; //handles the signal properly
@@ -63,6 +48,7 @@ int main(int argc, char *argv[]) {
         }
 
         for (; optind < argc; optind++) { //loops until the end of the argument to take in all the arguments
+
                 int p1[2], p2[2], pid1, pid2, status1, status2;
                 if (pipe(p1) < 0 || pipe(p2) < 0) { //p1 is from cat to grep and p2 is for grep to more
                         perror("Error creating pipes");
@@ -74,51 +60,41 @@ int main(int argc, char *argv[]) {
                         perror("Error forking child process");
                         return -1;
                 }
-                if (pid1 == 0) { //child process for grep
-                        if (pipeIO(p1, STDIN_FILENO) < 0 || pipeIO(p2, STDOUT_FILENO) < 0)
+                if (pid1 == 0) { //child process for more
+                        if (pipe_ops(STDIN_FILENO, p2) < 0)
                                 return -1;
 
-                        if (close(p1[0]) < 0 || close(p1[1]) < 0) {
-                                perror("Error closing the file descriptors of pipe between cat and grep");
+                        if (close_pipes(p1, p2) < 0)
                                 return -1;
-                        }
-                        if (close(p2[0]) < 0 || close(p2[1]) < 0) {
-                                perror("Error closing the file descriptors of pipe between grep and more");
-                                return -1;
-                        }
 
-                        if (execlp("grep", "grep", pattern, (char *)0) == -1) {
-                                fprintf(stderr, "Error running 'grep' program: %s\n", strerror(errno));
+                        if (execlp("more", "more") < 0) {
+                                perror("Error executing more");
                                 return -1;
                         }
-
 
 
                 } else {
+
+                        int bytes_r = 1, bytes_w = 1;
                         pid2 = fork();
                         if (pid2 < 0) {
                                 perror("Error forking child process");
                                 return -1;
                         }
-                        if (pid2 == 0) { //child process for more
-                                if (pipeIO(p2, STDIN_FILENO) < 0)
+                        if (pid2 == 0) { //child process for grep
+                                if (pipe_ops(STDIN_FILENO, p1) < 0 || pipe_ops(STDOUT_FILENO, p2) < 0)
                                         return -1;
 
-                                if (close(p1[0]) < 0 || close(p1[1]) < 0) {
-                                        perror("Error closing the file descriptors of pipe between cat and grep");
+                                if (close_pipes(p1, p2) < 0)
                                         return -1;
-                                }
-                                if (close(p2[0]) < 0 || close(p2[1]) < 0) {
-                                        perror("Error closing the file descriptors of pipe between grep and more");
-                                        return -1;
-                                }
-                                if (execlp("more", "more", (char *)0) == -1) {
-                                        fprintf(stderr, "Error running 'more' program: %s\n", strerror(errno));
-                                        return -1;
-                                }
 
+                                if (execlp("grep", "grep", pattern) < 0) {
+                                        perror("Error executing grep");
+                                        return -1;
+                                }
 
                         } else { //parent process for catgrepmore
+
                                 fd_in = open(argv[optind], O_RDONLY);
                                 if (fd_in < 0) {
                                         snprintf(err_msg, sizeof(err_msg), "Error opening input file %s", argv[optind]);
@@ -141,22 +117,17 @@ int main(int argc, char *argv[]) {
                                                 perror("Error writing to pipe between cat and grep");
                                                 return -1;
                                         }
-                                        if (bytes_w - bytes_r < 0) {
+                                        if (bytes_w - bytes_r < 0) { //Checks for partial writes
                                                 perror("Partial write occured");
                                                 return -1;
                                         }
                                 }
-                                if (close(p1[0]) < 0 || close(p1[1]) < 0) {
-                                        perror("Error closing the file descriptors of pipe between cat and grep");
+                                if (close_pipes(p1, p2) < 0)
                                         return -1;
-                                }
-                                if (close(p2[0]) < 0 || close(p2[1]) < 0) {
-                                        perror("Error closing the file descriptors of pipe between grep and more");
+
+                                if (waitpid(pid1, &status1, 0) < 0 || waitpid(pid2, &status2, 0) < 0)
                                         return -1;
-                                }
-                                if (waitpid(pid1, &status1, 0) < 0 || waitpid(pid2, &status2, 0) < 0) {
-                                        return -1;
-                                }
+
                                 if (WIFEXITED(status1) == 0 || WIFEXITED(status2) == 0) {
                                         perror("Error exiting grep or more normally");
                                         return -1;
@@ -164,4 +135,30 @@ int main(int argc, char *argv[]) {
                         }
                 }
         }
+}
+
+int close_pipes(int p1[], int p2[]) {
+        if (close(p1[0]) < 0 || close(p1[1]) < 0) {
+                perror("Error closing the file descriptors of pipe between cat and grep");
+                return -1;
+        }
+        if (close(p2[0]) < 0 || close(p2[1]) < 0) {
+                perror("Error closing the file descriptors of pipe between grep and more");
+                return -1;
+        }
+        return 0;
+}
+
+int pipe_ops(int fd, int pipe[]) { // redirects pipes to stdin or stdout
+        if (fd == STDIN_FILENO)
+                if (dup2(pipe[0], STDIN_FILENO) < 0) {
+                        perror("Error redirecting input");
+                        return -1;
+                }
+        if (fd == STDOUT_FILENO)
+                if (dup2(pipe[1], STDOUT_FILENO) < 0) {
+                        perror("Error redirecting ouput");
+                        return -1;
+                }
+        return 0;
 }
